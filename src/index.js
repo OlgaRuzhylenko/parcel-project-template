6e3b4e95a2d0ed443a39e561110eb19c1a3b0070
OlgaRuzhylenko <134257866+OlgaRuzhylenko@users.noreply.github.com>
2023-10-22 10:18:20 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import * as basicLightbox from 'basiclightbox'
+import "basiclightbox/dist/basicLightbox.min.css";
+
 const instruments = [
     {
         id: 1,
@@ -69,10 +72,12 @@ const list = document.querySelector('.js-list')
 
 function createMarkup(arr) {
     const markup = arr.map(({id, img, name}) => 
-`<li data-id=${id}>
+`<li data-id=${id} class="js-card">
 <img src="${img}" alt="${name}" width="300">
 <h2>${name} </h2>
-<p class="js-info"><a href="#">More information</a></p>
+<p>
+<a href="#" class="js-info">More information</a>
+</p>
 <div>
   <button>Add to favorite</button>
   <button>Add to basket</button>
@@ -85,6 +90,28 @@ list.innerHTML = markup;
 list.addEventListener('click', onClick);
 
 function onClick(evt) {
-
+evt.preventDefault();
+if (evt.target.classList.contains('js-info')) {
+    const { id } = evt.target.closest('.js-card').dataset;
+    const product = findProduct(Number(id));
+    const instance = basicLightbox.create(`
+	<div>
+    <img src="${product.img}" alt="${product.name}" width="300">
+    <h2>${product.name}</h2>
+    <h3>${product.price}</h3>
+    <p>${product.description}</p>
+    <div>
+      <button>Add to favorite</button>
+      <button>Add to basket</button>
+    </div>
+  </div>
+`);
+instance.show();
+   }
 }
-createMarkup(instruments)
+
+createMarkup(instruments);
+
+function findProduct(productId){
+    return instruments.find(({id}) => id === productId);
+}
